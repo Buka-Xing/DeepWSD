07a04ccb8497b36e034548f1ab20d2ebfd507e51
Xingran Liao <72482963+Buka-Xing@users.noreply.github.com>
2022-07-10 18:19:43 +0800

    Update DeepWSD.py

--- DeepWSD.py
+++ DeepWSD.py
@@ -179,8 +179,8 @@ if __name__ == '__main__':
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    ref = prepare_image(Image.open(args.ref).convert("RGB"),resize=False).to(device)
-    dist = prepare_image(Image.open(args.dist).convert("RGB"),resize=False).to(device)
+    ref = prepare_image(Image.open(args.ref).convert("RGB")).to(device)
+    dist = prepare_image(Image.open(args.dist).convert("RGB")).to(device)
 
     model = DeepWSD().to(device)
 
